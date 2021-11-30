0caecd7e4800bb524b0e13521e46acb69895bda2
Ignacio Betegón <nachogon92@gmail.com>
2021-11-30 21:30:36 +0100

    Add event api

---
+++ app/api/event.py
@@ -0,0 +1,12 @@
+from typing import List
+
+from fastapi import APIRouter
+
+from app.models.event import Event
+
+event_router = APIRouter()
+
+
+@event_router.post("/checker")
+def validate_event_schema(events: List[Event]):
+    return events
