d615bb8b19265634e83e655a9d4ed0c1d619485c
Ignacio Betegón <nachogon92@gmail.com>
2021-12-01 02:46:09 +0100

    Add event tools

---
+++ app/scripts/event_tools.py
@@ -0,0 +1,61 @@
+import json
+
+import click
+from pydantic import ValidationError
+
+from models.event import Event
+
+from loguru import logger
+
+import os
+
+from core.config import LOG_PATH
+
+
+@click.group()
+@click.pass_context
+def main(ctx):
+    """Script cli."""
+    logger.add(
+        f"{LOG_PATH}",
+        format="{time} {level} {extra[name]} {extra[time]} {message}",
+    )
+    ctx.obj = logger
+
+
+@main.command()
+@click.option("--file", help="Generate Report")
+def validate_json(file):
+    """Validate the events from the events of a file"""
+    with open(file) as file:  # Open file as an iterator to save memory.
+        for i, line in enumerate(file):
+            try:
+                Event(**json.loads(line))
+            except ValidationError as e:
+                logger.error(
+                    f"Validation error at event {i + 1} with errors {e.json()}"
+                )
+
+
+@main.command()
+@click.option("--file", help="Generate Report")
+@click.pass_context
+def generate_report(ctx, file):
+    """Create report from events file with name of event, date, count of events"""
+    logger = ctx.obj
+    count_dict = {}
+    with open(file) as file:  # Open file as an iterator to save memory.
+        for line in file:
+            try:
+                event = Event(**json.loads(line))
+                if count_dict.get(event.original_timestamp.date()):
+                    count_dict[event.original_timestamp.date()] += [event.id]
+                else:
+                    count_dict[event.original_timestamp.date()] = [event.id]
+            except ValidationError:
+                continue
+        for date in count_dict:
+            for id in count_dict[date]:
+                logger.bind(name=id, time=date.strftime("%m/%d/%Y")).info(
+                    f" has {len(count_dict[date])} events on the same date."
+                )
